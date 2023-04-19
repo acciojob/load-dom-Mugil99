1bd9b38bcd8c06a7badb5c8c404eca3544360faf
Mugil99 <124775550+Mugil99@users.noreply.github.com>
2023-04-19 15:02:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+  function onLoad() {
+        const body = document.querySelector('body');
+        body.innerHTML = 'DOM load success';
+      }
+
+window.addEventListener('DOMContentLoaded', onLoad);
